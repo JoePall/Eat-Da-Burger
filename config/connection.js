956ed2ef24d88cf9527078384fd80438b158632b
Josiah Powell <josiahpowell@outlook.com>
2020-09-10 23:11:03 -0500

    configuration for server

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 var connection = require("mysql").createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "SwimmingOtters357",
-    database: "burgers_db"
+    host: "z8dl7f9kwf2g82re.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "netxg6ya1hnzc2nf",
+    password: "lxsklu5fy4606ulb",
+    database: "mcqzrhl089dp6qkx",
+    dialect: "mysql"
 });
 
 connection.connect((err) => {
--- server.js
+++ server.js
@@ -6,12 +6,9 @@ const routes = require("./controllers/burgerController.js");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
 
 app.engine("handlebars", require("express-handlebars")({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.use(routes);
-
-app.listen(PORT, function() {
-  console.log(`started on http://localhost:${PORT}/`);
-});
+app.listen(PORT, () => console.log(`started on http://localhost:${PORT}/`));
