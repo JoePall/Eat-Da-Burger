47418ecfce7d00f144a9cc3007ed80e83519a99e
Josiah Powell <josiahpowell@outlook.com>
2020-09-10 23:23:22 -0500

    configured connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
-var connection = require("mysql").createConnection({
-    host: "z8dl7f9kwf2g82re.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "netxg6ya1hnzc2nf",
-    password: "lxsklu5fy4606ulb",
-    database: "mcqzrhl089dp6qkx",
-    dialect: "mysql"
-});
+var mysql = require("mysql");
+var connection;
 
-connection.connect((err) => {
-    if (err) console.error("error connecting: " + err.stack);
-    else console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = require("mysql").createConnection({
+        host: "localhost",
+        port: "3306",
+        user: "root",
+        password: "SwimmingOtters357",
+        database: "burgers_db"
+    });
+}
 
+connection.connect();
 module.exports = connection;
