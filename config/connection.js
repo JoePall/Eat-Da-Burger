2a10fa8f9e36a4032c0c3f8376bc64e2a080ac18
Josiah Powell <josiahpowell@outlook.com>
2020-10-29 16:15:52 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,13 +5,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
-    connection = require("mysql").createConnection({
-        host: "localhost",
-        port: "3306",
-        user: "root",
-        password: "SwimmingOtters357",
-        database: "burgers_db"
-    });
+    
 }
 
 connection.connect();
